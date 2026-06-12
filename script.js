4473cfaa1bc77e3f727599d07fc75d3e137765fe
Pavan <unknownnamer2000@gmail.com>
2026-06-12 22:09:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+document.getElementById('palak-paneer-btn').addEventListener('click', function() {
+        window.location.href = 'palak-paneer.html';
+    });
+
+document.getElementById('chole-btn').addEventListener('click', function() {
+        window.location.href = 'chole.html';
+    });
+
+document.getElementById('aloo-gobi-btn').addEventListener('click', function() {
+        window.location.href = 'aloo-gobi.html';
+    });
+
+document.getElementById('dal-tadka-btn').addEventListener('click', function() {
+        window.location.href = 'dal-tadka.html';
+    });
+
+document.getElementById('sambar-btn').addEventListener('click', function() {
+        window.location.href = 'sambar.html';
+    });
+
+document.getElementById('upma-btn').addEventListener('click', function() {
+        window.location.href = 'upma.html';
+    });
+
+document.getElementById('pongal-btn').addEventListener('click', function() {
+        window.location.href = 'pongal.html';
+    });
+
+document.getElementById('egg-curry-btn').addEventListener('click', function() {
+        window.location.href = 'egg-curry.html';
+    });
+
